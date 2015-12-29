02edb2f2d1b9081cb46d7ac7e911cd3f67b99372
unknown <>
2015-12-29 15:53:32 +0800

    compete

---
+++ peachview/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace peachview
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow myMainWindow = new MainWindow(e.Args);
+            myMainWindow.Show();
+        }
+    }
+}
---
+++ peachview/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MessageBox = System.Windows.MessageBox;
+
+namespace peachview
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow(string[] args)
+        {
+            this._args = args;
+            InitializeComponent();
+        }
+
+        private string[] _args;
+
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                System.Windows.Forms.Screen screen = Screen.PrimaryScreen;
+                System.Drawing.Rectangle rct = screen.Bounds;
+#if DEBUG
+                //this._args=new string[]{@"F:\100.png"};
+#endif
+                if (this._args != null && this._args.Length > 0)
+                {
+                    BitmapImage image = new BitmapImage(new Uri(this._args[0], UriKind.Absolute));
+                    this.ImgMain.Source = image;
+                    this.Width = image.PixelWidth > rct.Width ? rct.Width : image.PixelWidth;
+                    this.Height = image.PixelHeight > rct.Height ? rct.Height : image.PixelHeight;
+                    this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+                    this.Topmost = true;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("请重试,出错了.", "呵呵", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
